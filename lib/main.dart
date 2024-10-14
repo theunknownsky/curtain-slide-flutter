055ca2316ca43d156c6f30ef7af69c19c7e3ea72
theunknownsky <vonnmamaril24@gmail.com>
2024-10-14 23:58:28 +0800

    login page frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,186 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const CurtainSlideApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CurtainSlideApp extends StatelessWidget {
+  const CurtainSlideApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      home: BufferPage(title: 'CurtainSlide'),
+    );
+  }
+}
+
+class BufferPage extends StatefulWidget {
+  const BufferPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<BufferPage> createState() => _BufferPageState();
+}
+
+class _BufferPageState extends State<BufferPage> {
+  @override
+  Widget build(BuildContext context) {
+    return RegisterPage(title: widget.title);
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return RegisterPageWidget(title: widget.title);
+  }
+}
+
+class RegisterPageWidget extends StatefulWidget {
+  const RegisterPageWidget({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<RegisterPageWidget> createState() => _RegisterPageWidgetState();
+}
+
+class _RegisterPageWidgetState extends State<RegisterPageWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(widget.title),
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100.0),
+          child: Column(
+            children: [
+              const Text(
+                "Welcome to",
+                style: TextStyle(
+                  fontSize: 36,
+                ),
+              ),
+              const Text(
+                "CurtainSlide",
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 50),
+                child: const Text(
+                  "Enter CurtainSlide Credentials",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(40, 8, 40, 0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "CurtainSlide ID",
+                        ),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a CurtainSlide ID';
+                          } else if (!value
+                              .startsWith(RegExp(r'^ctnlsd-\d{9}$'))) {
+                            return 'Please enter a valid CurtainSlide ID';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(40, 8, 40, 0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          labelText: "CurtainSlide Password",
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscureText
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                          ),
+                        ),
+                        obscureText: _obscureText,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0),
+                      child: FilledButton(
+                        onPressed: () {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            // Process data.
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            Colors.green.shade600,
+                          ),
+                          padding: WidgetStateProperty.all<EdgeInsets>(
+                            const EdgeInsets.symmetric(horizontal: 140),
+                          ),
+                          shape: WidgetStateProperty.all<OutlinedBorder>(
+                            const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                          ),
+                        ),
+                        child: const Text(
+                          'Submit',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
