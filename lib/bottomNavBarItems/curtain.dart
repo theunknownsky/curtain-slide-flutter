e07809c330fa0dba2017adf467fa5b7ea222b0f7
theunknownsky <vonnmamaril24@gmail.com>
2024-10-24 13:39:07 +0800

    opening and closing curtain stored in firebase db

--- lib/bottomNavBarItems/curtain.dart
+++ lib/bottomNavBarItems/curtain.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class CurtainWidget extends StatefulWidget {
   const CurtainWidget({super.key});
@@ -8,14 +11,101 @@ class CurtainWidget extends StatefulWidget {
 }
 
 class _CurtainWidgetState extends State<CurtainWidget> {
+  final TextStyle _tStyle = TextStyle(fontSize: 22);
+
+  bool actionBlock = false;
+
+  void _curtainStateChange(int curtainMoveState) {
+    FirebaseFirestore.instance
+        .collection('users') // Replace with your collection name
+        .doc(
+            FirebaseAuth.instance.currentUser!.uid) // Get the current user's ID
+        .update({'curtainState': curtainMoveState});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "This is the Curtain Body",
-        style: TextStyle(
-          fontSize: 30,
+    if (actionBlock) {
+      return AbsorbPointer(
+        child: Center(
+          child: Column(
+            children: [
+              CircularProgressIndicator(),
+              Text(
+                "Curtain operation in progress.",
+                style: _tStyle,
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
         ),
+      );
+    }
+    _curtainStateChange(1);
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                "Close Curtain",
+                style: _tStyle,
+              ),
+              FilledButton.icon(
+                label: Row(
+                  children: [
+                    Icon(Icons.curtains_closed),
+                    Text("Close")
+                  ],
+                ),
+                onPressed: () {
+                  setState(() {
+                    actionBlock = true;
+                  });
+                  _curtainStateChange(0);
+                  Timer(Duration(seconds: 3), () {
+                    setState(() {
+                      actionBlock = false;
+                    });
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.black),
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "Open Curtain",
+                style: _tStyle,
+              ),
+              FilledButton.icon(
+                label: Row(
+                  children: [
+                    Icon(Icons.curtains),
+                    Text("Open")
+                  ],
+                ),
+                onPressed: () {
+                  setState(() {
+                    actionBlock = true;
+                  });
+                  _curtainStateChange(2);
+                  Timer(Duration(seconds: 3), () {
+                    setState(() {
+                      actionBlock = false;
+                    });
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(Colors.black),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
